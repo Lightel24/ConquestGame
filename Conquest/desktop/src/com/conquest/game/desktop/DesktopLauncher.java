6111b89c52ec4bf18e5426658701b656d95ca238
Lightel24 <Lightel24@gmail.com>
2020-07-17 14:55:13 +0200

    Commit initial

---
+++ Conquest/desktop/src/com/conquest/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.conquest.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.conquest.game.ConquestGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ConquestGame(), config);
+	}
+}
